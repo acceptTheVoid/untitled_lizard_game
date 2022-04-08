8780e0f7d9ddc8470ca11d16fd931cf1e4a0b6dd
acceptTheVoid <arcad2004@gmail.com>
2022-04-08 15:08:27 +1000

    Better movement

--- Assets/Scripts/BetterJump.cs
+++ Assets/Scripts/BetterJump.cs
@@ -1,25 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BetterJump : MonoBehaviour
-{
+public class BetterJump : MonoBehaviour {
     private Rigidbody2D _rb;
     [SerializeField] private float fallMultiplier = 2.5f;
     [SerializeField] private float lowJumpMultiplayer = 2f;
     
-    void Start()
-    {
+    private void Start() {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        if (_rb.velocity.y < 0)
-        {
+    private void Update() {
+        if (_rb.velocity.y < 0) {
             _rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        } else if (_rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
+        } else if (_rb.velocity.y > 0 && !Input.GetButton("Jump")) {
             _rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplayer - 1) * Time.deltaTime;
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,38 +1,49 @@
-using Unity.Mathematics;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private Rigidbody2D _rb;
+    private BoxCollider2D _bc;
+    
+    [SerializeField] private LayerMask platformsLayerMask;
     [SerializeField] private float speed = 10f;
     [SerializeField] private float jumpVelocity = 5f;
 
-    private void Start()
-    {
+    private void Start() {
         _rb = GetComponent<Rigidbody2D>();
+        _bc = GetComponent<BoxCollider2D>();
     }
 
-    private void Update()
-    {
+    private void Update() {
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
         var dir = new Vector2(x, y);
 
-        if (Input.GetButtonDown("Jump") && math.abs(_rb.velocity.y) < 0.1)
-        {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
             Jump();
         }
         
         Walk(dir);
     }
 
-    private void Walk(Vector2 dir)
-    {
-        _rb.velocity = (new Vector2(dir.x * speed, _rb.velocity.y));
+    private void Walk(Vector2 dir) {
+        _rb.velocity = new Vector2(dir.x * speed, _rb.velocity.y);
     }
 
-    private void Jump()
-    {
+    private void Jump() {
         _rb.velocity += Vector2.up * jumpVelocity;
+    }
+
+    private bool IsGrounded() {
+        var bounds = _bc.bounds;
+        var raycast = Physics2D.BoxCast(
+            bounds.center, 
+            bounds.size, 
+            0f, 
+            Vector2.down,
+            .1f,
+            platformsLayerMask
+            );
+        Debug.Log(raycast.collider);   
+        return raycast.collider != null;
     }
 }
